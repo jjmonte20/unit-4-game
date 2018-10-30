723af80461b5f1b0541f0b2478c855e23c1197a7
jjmonte20 <jjmonte20@gmail.com>
2018-10-30 08:55:35 -0500

    currently have buttons that will add to currentValue, still need to fix win conditions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,25 +30,70 @@
 
 // Define variables for the initial score
 var currentValue = 0;
-var firstButton;
-var secondButton;
-var thirdButton;
-var fourthButton;
 
+// goal number variable needs to be a random number between 19 and 120
+var goalNumber = Math.floor((Math.random() * 120) + 19);
+console.log(goalNumber);
+
+//now I need to give the buttons a value.  These are valued at a random number between 1 and 12
+var firstButton = Math.floor((Math.random() * 12) + 1);
+console.log("First button value is: " + firstButton);
+
+//button 2
+var secondButton = Math.floor((Math.random() * 12) + 1);
+console.log("Second button value is: " + secondButton);
+
+//button 3
+var thirdButton = Math.floor((Math.random() * 12) + 1);
+console.log("Third button value is: " + thirdButton);
+
+//button 4
+var fourthButton = Math.floor((Math.random() * 12) + 1);
+console.log("Fourth button value is: " + fourthButton);
+
+    //first task make buttons that work.  .on("click", function{}) should do the trick
     //buttons work and each have unique functions
     $(".btn-primary").on("click", function() {
         console.log("Button 1 works");
+        //now that I know the button works.  I need to add value to the button.
+        //I have a value for the first button, now I should add it to the current value.
+        currentValue = currentValue + firstButton;
+        console.log(currentValue);
     });
     $(".btn-secondary").on("click", function() {
         console.log("Button 2 works");
+        //was able to successfully add a value to current value, but need to ensure the value retains its number when I enter the others
+        //button 2 function
+        currentValue = currentValue + secondButton;
+        console.log(currentValue);
     });
     $(".btn-success").on("click", function() {
         console.log("Button 3 works");
+        //button 3 function
+        currentValue = currentValue + thirdButton;
+        console.log(currentValue);
     });
     $(".btn-danger").on("click", function() {
         console.log("Button 4 works");
+        //button 4 function
+        currentValue = currentValue + fourthButton;
+        console.log(currentValue);
     });
 
 
+    //now that these buttons work, I need to add some game logic.
+    //I have a goal value that picks a random number between 19 and 120
+    //I want the win condition to be a matching number between the current value and the goal number
+    //I want the losing condition to be the current number being over the goal number
 
-//first task make buttons that work.  .on("click", function{}) should do the trick
+    //function for if the person wins
+    if (currentValue == goalNumber) {
+        alert("You win!!!");
+        console.log("win");
+    } 
+    
+    //function for if the person loses
+    else if (currentValue > goalNumber) {
+        alert("You lose!!!");
+        console.log("lose");
+    }
