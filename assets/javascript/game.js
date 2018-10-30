b51ebb0aec820bb129219fa27e33c5dce9c8a211
jjmonte20 <jjmonte20@gmail.com>
2018-10-30 08:17:17 -0500

    Added clickable buttons, currently they console.log phrases that show that work individually

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+// Watch the demo.
+
+// The player will have to guess the answer, just like in Word Guess. This time, though, the player will guess with numbers instead of letters.
+
+// Here's how the app works:
+
+// There will be four crystals displayed as buttons on the page.
+
+// The player will be shown a random number at the start of the game.
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+
+// Your game will hide this amount until the player clicks a crystal.
+// When they do click one, update the player's score counter.
+// The player wins if their total score matches the random number from the beginning of the game.
+
+// The player loses if their score goes above the random number.
+
+// The game restarts whenever the player wins or loses.
+
+// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+// Option 1 Game design notes
+// The random number shown at the start of the game should be between 19 - 120.
+
+// Each crystal should have a random hidden value between 1 - 12.
+
+
+
+// Define variables for the initial score
+var currentValue = 0;
+var firstButton;
+var secondButton;
+var thirdButton;
+var fourthButton;
+
+    //buttons work and each have unique functions
+    $(".btn-primary").on("click", function() {
+        console.log("Button 1 works");
+    });
+    $(".btn-secondary").on("click", function() {
+        console.log("Button 2 works");
+    });
+    $(".btn-success").on("click", function() {
+        console.log("Button 3 works");
+    });
+    $(".btn-danger").on("click", function() {
+        console.log("Button 4 works");
+    });
+
+
+
+//first task make buttons that work.  .on("click", function{}) should do the trick
